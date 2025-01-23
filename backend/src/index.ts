40de077db3d6624e6ce9c3a1f51ef176f7317602
Ace G <aceguevarra48@gmail.com>
2025-01-23 13:45:38 +0800

    refactor: completed webhook functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,28 +3,34 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import type { WebhookPayload } from "@/types/index.js";
 import supabase from "@/supabase.js";
+import { logger } from "hono/logger";
 
 const WEBHOOK_SIGNATURE = process.env.WEBHOOK_SIGNATURE as string;
 const app = new Hono();
 
+let RECORD = {};
+
+app.use(logger());
+
 app.get("/", (c) => {
+    console.log(RECORD);
     return c.text("Hello Hono!");
 });
 
 app.post("/supabase/webhook/user-create", async (c) => {
     try {
-        const { record } = await c.req.json<WebhookPayload>();
-        if (record.email_confirmed_at == null) {
-            c.status(200);
-            return c.json({ message: "Webhook received" });
-        }
-
-        await supabase.from("users").insert({
-            id: record.id,
-            email: record.email,
-            username: record.raw_user_meta_data.username,
-            password: record.encrypted_password,
-        });
+        const data = await c.req.json<WebhookPayload>();
+        // if (record.email_confirmed_at == null) {
+        //     c.status(200);
+        //     return c.json({ message: "Webhook received" });
+        // }
+        RECORD = data;
+        // await supabase.from("users").insert({
+        //     id: record.id,
+        //     email: record.email,
+        //     username: record.raw_user_meta_data.username,
+        //     password: record.encrypted_password,
+        // });
 
         c.status(201);
         return c.json({ message: "User has been added" });
