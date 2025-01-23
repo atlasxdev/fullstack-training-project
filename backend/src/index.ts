e53a0f16c7b8fb867983be8f875fca61537fdc7c
Ace G <aceguevarra48@gmail.com>
2025-01-23 11:57:42 +0800

    fix: webhook route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,27 +5,21 @@ import { Hono } from "hono";
 const WEBHOOK_SIGNATURE = process.env.WEBHOOK_SIGNATURE as string;
 const app = new Hono();
 
+let DATA = {};
+
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
 
 app.get("/hello", (c) => {
     console.log("YES");
-    return c.text("Hello PO!");
+    return c.json({ DATA });
 });
 
 app.post("/supabase/webhook/user-create", async (c) => {
-    const signature = c.req.header("Authorization")?.split(" ")[1];
-
-    if (!signature || signature != WEBHOOK_SIGNATURE) {
-        c.status(401);
-        return c.json({
-            error: "Unauthorized access!",
-            message: "Invalid token signature",
-        });
-    }
     const data = await c.req.json();
-    return c.json({ data });
+    DATA = data;
+    return c.json({ DATA });
 });
 
 const port = 3000;
