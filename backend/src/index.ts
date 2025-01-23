1495f5439cea2f755c751d460ee7bd18cc1df6be
Ace G <aceguevarra48@gmail.com>
2025-01-23 11:22:43 +0800

    feat: added a webhook route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
+const WEBHOOK_SIGNATURE = process.env.WEBHOOK_SIGNATURE as string;
 const app = new Hono();
 
 app.get("/", (c) => {
     return c.text("Hello Hono!");
+});
+
+app.post("/supabase/webhook/user-create", async (c) => {
+    const signature = c.req.header("Authorization")?.split(" ")[1];
+
+    if (!signature || signature != WEBHOOK_SIGNATURE) {
+        c.status(401);
+        return c.json({
+            error: "Unauthorized access!",
+            message: "Invalid token signature",
+        });
+    }
+    const data = await c.req.json();
+    return c.json({ data });
 });
 
 const port = 3000;
