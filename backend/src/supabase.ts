dbd57df2ebf4eb1df9e4afef4c8f953293ba5eb2
Ace G <aceguevarra48@gmail.com>
2025-01-23 13:09:24 +0800

    refactor: completed webhook functionality

--- backend/src/supabase.ts
+++ backend/src/supabase.ts
@@ -4,6 +4,9 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = process.env.SUPABASE_URL as string;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY as string;
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(
+    "https://dioketiuedohqlaegzhm.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpb2tldGl1ZWRvaHFsYWVnemhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NDI4NTksImV4cCI6MjA1MzExODg1OX0.aOl2aPqWDCvzc1eCenT2cqkSPw87zDc36WB9x1dyxs4"
+);
 
 export default supabase;
