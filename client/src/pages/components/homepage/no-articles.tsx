2ee97e16e34e5dda387ba19721aa218ea6ac7ce6
Ace G <aceguevarra48@gmail.com>
2025-02-02 15:36:16 +0800

    style: removed unnecessary elements

--- client/src/pages/components/homepage/no-articles.tsx
+++ client/src/pages/components/homepage/no-articles.tsx
@@ -1,8 +1,23 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { Link } from "react-router";
-import { buttonVariants } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+import EditorLoader from "./editor-loader";
 
 function NoArticles() {
+    const navigate = useNavigate();
+    const [isNavigating, setTransition] = useTransition();
+
+    if (isNavigating) {
+        return <EditorLoader />;
+    }
+
+    function navigateToCreateArticle() {
+        setTransition(() => {
+            navigate("/create-article");
+        });
+    }
+
     return (
         <Card className="bg-secondary py-16">
             <CardHeader className="flex flex-col items-center w-max mx-auto">
@@ -12,14 +27,9 @@ function NoArticles() {
                     src="/no-articles.png"
                     className="size-56 object-cover"
                 />
-                <Link
-                    to={"/create-article"}
-                    className={buttonVariants({
-                        variant: "default",
-                    })}
-                >
-                    Create a new article
-                </Link>
+                <Button onClick={() => navigateToCreateArticle()}>
+                    New article
+                </Button>
             </CardHeader>
         </Card>
     );
